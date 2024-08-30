2c0c1660ea9a58317ca674aee6b32ebb8211ba05
Ludis <leulsegedmelaku1020@gmail.com>
2024-08-30 12:20:10 +0300

    "Update title in index.html and add Router to App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { BrowserRouter as Router } from "react-router-dom";
+
 import { AuthenticationRoute } from "./routes/AuthenticationRoute";
 
 function App() {
-  return <AuthenticationRoute />;
+  return (
+    <Router>
+      <AuthenticationRoute />
+    </Router>
+  );
 }
 
 export default App;
