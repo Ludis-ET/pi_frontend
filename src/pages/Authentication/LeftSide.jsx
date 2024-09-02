19da5b4d432520aad604f36a680dc748c4d6fa76
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-02 15:07:56 +0300

    Improve LeftSide component layout and add lazy loading for images

--- src/pages/Authentication/LeftSide.jsx
+++ src/pages/Authentication/LeftSide.jsx
@@ -1,4 +1,4 @@
-import Image from "../../assets/phone-login.jpg"
+import Image from "../../assets/phone-login.jpg";
 
 export const LeftSide = () => {
   return (
@@ -12,10 +12,17 @@ export const LeftSide = () => {
         </div>
       </div>
 
-      <div className="hidden mockup-phone border-primary absolute bottom-[5%] left-[20%] transform rotate-90">
+      <div className="mockup-phone border-primary absolute h-[600px] w-[350px] bottom-[-5%] left-[20%] transform rotate-90">
         <div className="camera"></div>
         <div className="display">
-          <div className="artboard artboard-demo phone-1">Hi.</div>
+          <div className="absolute bg-white top-[135px] rounded-[40px] right-[-115px] h-[320px] w-[565px] flex flex-col justify-center items-left p-6 transform rotate-[270deg]">
+            <h1 className="text-2xl font-bold text-purple-500">Pi Parent's Portal</h1>
+            <p className="text-lg mt-4">
+              An innovation solution tailored for seamless school management.
+              Experience efficiency & precision in administrative tasks.
+              Real-time student monitoring and insightful data analytics.{" "}
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -11,7 +11,7 @@ export const Login = () => {
       <div className="absolute hidden xl:block bg-purple-500 w-[600px] h-[600px] rounded-[100%] left-[-10%] top-[-20%]"></div>
       <div className="absolute flex flex-wrap justify-center md:right-[15%] md:top-[15%] z-10">
         <div className="absolute bg-purple-500 w-[300px] h-[300px] md:w-[600px] md:h-[600px] rounded-[100%] right-[-20%] top-[-10%] md:right-[-80%] md:top-[-50%]"></div>
-        <div className="absolute bg-purple-500 w-[300px] h-[300px] md:w-[600px] md:h-[600px] rounded-[100%] left-[-20%] bottom-[-30%] md:left-[-80%] md:bottom-[-50%]"></div>
+        <div className="absolute bg-purple-500 w-[300px] h-[300px] md:w-[600px] md:h-[600px] rounded-[100%] left-[-20%] bottom-[-30%] md:left-[-80%] md:bottom-[-60%]"></div>
         <div className="mt-20 py-10 px-5 md:mt-0 md:ml-12 w-[80%] h-[90%] md:w-[500px] md:h-[650px] bg-gray-100/30 rounded-md backdrop-blur-md border border-white/20 shadow-xl">
           <header className="text-[30px] flex flex-col justify-center items-center font-bold font-serif text-center p-4 ">
             <img src={Logo} className="w-[300px]" />
