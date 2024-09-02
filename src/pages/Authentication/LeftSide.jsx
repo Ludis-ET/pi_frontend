f948bea8302e031bbabaf24a85e57aa68dad9234
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-02 14:27:20 +0300

    feat: Improve LeftSide component layout and add lazy loading for images

--- src/pages/Authentication/LeftSide.jsx
+++ src/pages/Authentication/LeftSide.jsx
@@ -3,16 +3,16 @@ import Image from "../../assets/phone-login.jpg"
 export const LeftSide = () => {
   return (
     <div className="w-1/2">
-      <div className="mockup-phone absolute top-[10%] left-[10%]">
+      <div className="mockup-phone w-[400px] h-[650px] absolute top-[10%] left-[10%]">
         <div className="camera"></div>
         <div className="display">
-          <div className="artboard artboard-demo phone-1">
+          <div className="artboard artboard-demo">
             <img src={Image} alt="" />
           </div>
         </div>
       </div>
 
-      <div className="mockup-phone border-primary absolute top-[20%] left-[20%]">
+      <div className="hidden mockup-phone border-primary absolute bottom-[5%] left-[20%] transform rotate-90">
         <div className="camera"></div>
         <div className="display">
           <div className="artboard artboard-demo phone-1">Hi.</div>
--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -5,11 +5,13 @@ import Logo from "../../assets/logo.png";
 export const Login = () => {
   return (
     <div className="w-full h-full m-0 p-0 absolute overflow-hidden flex flex-wrap gap-12 bg-white text-black">
-      <LeftSide />
-      <div className="absolute flex flex-wrap justify-left right-[15%] top-[15%] z-10">
+      <div className="hidden xl:block">
+        <LeftSide />
+      </div>
+      <div className="absolute flex flex-wrap justify-center md:right-[15%] md:top-[15%] z-10">
         <div className="absolute bg-purple-500 w-[300px] h-[300px] rounded-[100%] right-[-30%] top-[-20%]"></div>
         <div className="absolute bg-purple-500 w-[300px] h-[300px] rounded-[100%] left-[-30%] bottom-[-20%]"></div>
-        <div className="ml-12 w-[500px] h-[650px] bg-gray-100/30 rounded-md backdrop-blur-md border border-white/20 shadow-xl">
+        <div className="mt-20 py-10 px-5 md:mt-0 md:ml-12 w-[80%] h-[90%] md:w-[500px] md:h-[650px] bg-gray-100/30 rounded-md backdrop-blur-md border border-white/20 shadow-xl">
           <header className="text-[30px] flex flex-col justify-center items-center font-bold font-serif text-center p-4 ">
             <img src={Logo} className="w-[300px]" />
             Login
