00c51e19874a51f1fd415055eb9643c6e1e91a2d
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-07 13:34:53 +0300

    Refactor index.js and HomeRoute.jsx to add Discussion component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { Login } from "./Authentication/Login";
 export { Home } from "./Home/Home";
+export { Discussion } from "./Chat/Discussion";
--- src/routes/HomeRoute.jsx
+++ src/routes/HomeRoute.jsx
@@ -1,6 +1,6 @@
 import { LeftBar, RightBar } from "../utils";
 import { Header } from "../components/Header";
-import { Home } from "../pages/Home/Home";
+import { Home, Discussion } from "../pages";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const HomeRoute = () => {
@@ -11,6 +11,7 @@ export const HomeRoute = () => {
         <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route path="/discussion" element={<Discussion />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
