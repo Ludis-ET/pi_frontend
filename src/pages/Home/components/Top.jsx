26a95a1175b12ab39f27a8db092ab25e0f139ded
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-19 21:26:44 +0300

    Refactor Top component to display student's average performance gauge

--- src/pages/Home/components/Top.jsx
+++ src/pages/Home/components/Top.jsx
@@ -4,11 +4,11 @@ import { StudentContext } from "../../../context";
 
 export const Top = () => {
   const { student } = useContext(StudentContext);
-
+console.log(student)
   return (
     <div className="w-full flex justify-center items-center">
       <div className="flex gap-2">
-        <PerformanceGauge progress={80} />
+        <PerformanceGauge progress={student ? student.average : 0} />
         <div className="bg-gray-300 dark:text-[#333] p-4 flex flex-col gap-4 justify-center items-center rounded-lg w-32 lg:w-56 h-40">
           <p>Rank</p>
           <b className="lg:text-2xl flex justify-center items-center">
