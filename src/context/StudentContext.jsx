c680f249963adc0ffb452f5db4ea875fd2839e6f
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-19 20:28:53 +0300

    Refactor StudentProvider to include useEffect for fetching student data

--- src/context/StudentContext.jsx
+++ src/context/StudentContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
 import { AuthContext } from "./AuthenticationContext";
 
@@ -8,33 +8,43 @@ export const StudentContext = createContext();
 
 export const StudentProvider = ({ children }) => {
   const { myprofile } = useContext(AuthContext);
-  const [students, setStudents] = useState({});
+  const [students, setStudents] = useState([]);
   const [currentStudent, setCurrentStudent] = useState(null);
 
-  const getStudent = async () => {
-    try {
-      let response = await Promise.race([
-        fetch(`${backendUrl}user/students/?parent=${myprofile.phone}`),
-        new Promise((_, reject) =>
-          setTimeout(() => reject(new Error("Request timed out")), 20000)
-        ),
-      ]);
+  useEffect(() => {
+    const getStudent = async () => {
+      try {
+        let response = await Promise.race([
+          fetch(`${backendUrl}user/students/?parent=${myprofile.phone}`),
+          new Promise((_, reject) =>
+            setTimeout(() => reject(new Error("Request timed out")), 20000)
+          ),
+        ]);
 
-      let data = await response.json();
-      if (response.status === 200) {
-        setStudents(data);
-      } else {
-        toast.error(data.detail);
+        let data = await response.json();
+        if (response.status === 200) {
+          setStudents(data);
+        } else {
+          toast.error(data.detail);
+        }
+      } catch (error) {
+        toast.error(error.message);
       }
-    } catch (error) {
-      toast.error(error.message);
-    }
-  };
+    };
 
-  if (myprofile && !students) {
-    getStudent();
-  }
-  console.log(students);
+    if (myprofile && students.length === 0) {
+      getStudent();
+    }
+  }, [myprofile, students]);
+
+  useEffect(() => {
+    if (students.length > 0 && !currentStudent) {
+      setCurrentStudent(students[0]);
+    }
+  }, [students, currentStudent]);
+  
+
+  
   return (
     <StudentContext.Provider value=""> {children}</StudentContext.Provider>
   );
