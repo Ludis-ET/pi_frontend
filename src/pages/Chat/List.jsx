abe21d5581add5ce4d34eb93c0a801ca4694bb9d
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-20 20:10:49 +0300

    Refactor profile picture in Chat List component

--- src/pages/Chat/List.jsx
+++ src/pages/Chat/List.jsx
@@ -38,7 +38,11 @@ export const List = () => {
                 <div className="w-12 h-12 relative">
                   <img
                     className="w-12 h-12 rounded-full mx-auto"
-                    src="https://imgs.search.brave.com/_J8N2UDJaL5yqzSP7yAjwI_ndgX4FGOZW3aCDME6dd4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9ib3ktd2l0aC1z/aGlydC10aGF0LXNh/eXMtaGUtaXMtd2Vh/cmluZy1zaGlydC10/aGF0LXNheXMtaGUt/aXMtd2VhcmluZy1z/aGlydF8xMTQ2Nzkx/LTQ5Mi5qcGc_c2l6/ZT02MjYmZXh0PWpw/Zw"
+                    src={
+                      teacher.profile_pic
+                        ? teacher.profile_pic
+                        : "https://imgs.search.brave.com/JAHeWxUYEwHB7KV6V1IbI9oL7wxJwIQ4Sbp8dHQL09A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMjAx/MzkxNTc2NC9waG90/by91c2VyLWljb24t/aW4tZmxhdC1zdHls/ZS5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9UEotMnZvUWZh/Q3hhZUNsdzZYYlVz/QkNaT3NTTjlIVWVC/SUg1Qk82VmRScz0"
+                    }
                     alt="chat-user"
                   />
                   <span className="absolute w-4 h-4 bg-green-400 rounded-full right-0 bottom-0 border-2 border-white"></span>
@@ -49,16 +53,8 @@ export const List = () => {
                   <span className="font-semibold">{teacher.full_name}</span>
                 </div>
                 <div>
-                  <small className="text-gray-600">Yea, Sure!</small>
-                </div>
-              </div>
-              <div className="flex-2 text-right">
-                <div>
-                  <small className="text-gray-500">15 April</small>
-                </div>
-                <div>
-                  <small className="text-xs bg-purple-500 text-white rounded-full h-6 w-6 leading-6 text-center inline-block">
-                    23
+                  <small className="text-gray-600">
+                    {teacher.subject.name} Teacher
                   </small>
                 </div>
               </div>
